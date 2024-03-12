0399c19b9562bc48aaf929f19f9282599d1415f0
ian36410 <59251710+ian36410@users.noreply.github.com>
2024-03-13 02:28:09 +0800

    Add files via upload

---
+++ HW1.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int number; // The number of scores read.
+    int sum; // The summation of all the scores read.
+    int grade; // Holds each individual score as it is read from the keyboard.
+    float average; // Holds the computed average.
+    string str;
+    number = 0; // Initialize number and sum to 0 before starting.
+    sum = 0;
+    average = 0;
+    cout << "Please input your score : ";
+    // Infinite Loop.
+    while (1) {
+        cin >> str; // Get the user input.
+        //There will have exception when user input a string or char.
+        try {
+            //Check the input is an integer or not.
+            if (str.find(".") != string::npos || !stoi(str)) {
+                cout << "The score is invaild! Please input the right score !" << endl;
+                return -1;
+            }
+            else {
+                grade = stoi(str);
+                if (grade >= 0 && grade <= 100) {
+                    sum = sum + grade; // Add new score into sum
+                    number = number + 1; // and count one more grade
+                    cout << "Please input the next score or input -1 to show the average :";
+                    continue;
+                }
+                // When done reading scores, compute average and display.
+                else if (grade == -1) {
+                    average = (float)sum / number;
+                    cout << "Your average score is : " << average << endl;
+                    // When done, exit the program.
+                    return 0;
+                }
+            }
+        }
+        //Catch the exception and handle it.
+        catch (exception& e) {
+            cout << "The score is invaild! Please input the right score !" << endl;
+            return -1;
+        }
+    }
+}
---
+++ HW2.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+
+int getDigit(int number) {
+    int q = number / 10;
+    if (q != 0) {
+        return q + number % 10;
+    }
+    else {
+        return number;
+    }
+}
+
+int sumofDoubleEvenPlace(const string& cardNumber) {
+    int l = cardNumber.length();
+    int sum = 0;
+    for (int i = l - 2; i >= 0; i -= 2) {
+        int d = (cardNumber[i] - '0') * 2;
+        sum += getDigit(d);
+    }
+    return sum;
+}
+
+int sumOfOddPlace(const string& cardNumber) {
+    int l = cardNumber.length();
+    int sum = 0;
+    for (int i = l-1; i >= 0; i -= 2) {
+        sum += (cardNumber[i] - '0');
+    }
+    return sum;
+}
+
+bool startsWith(const string& cardNumber, const string& substr) {
+    return true;
+}
+
+bool isvalid(const string& cardNumber) {
+    int sum = sumOfOddPlace(cardNumber) + sumofDoubleEvenPlace(cardNumber);
+    if (sum % 10 != 0) {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+int main(int args, char** argv) {
+    const string a = "4388576018402626";
+    cout << isvalid(a) << endl;
+    return 0;
+}
